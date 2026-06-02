aa74196255bd0da7027db97c833d72c3330dac1d
Akika <gemingjia0201@163.com>
2026-06-02 15:21:25 +0800

    support https

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), basicSsl()],
   server: {
+    https: true,
     host: '0.0.0.0',
     port: 3007,
     proxy: {
